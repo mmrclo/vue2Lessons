462df2db1dad9575ee0469450d29e334e40a57a3
mmrclo <marcelo.kovalink@gmail.com>
2020-09-30 12:37:51 -0300

    External link a href add

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var app = new Vue({
         product: "Socks",
         description: "Socks scks ? .... !-!",
         image: "./images/vmSocks-green-onWhite.jpg",
-        altText: "Pair of Socks"
+        altText: "Pair of Socks",
+        outSidesrc: "https://en.wikipedia.org/wiki/Sock"
     }
 
 })
